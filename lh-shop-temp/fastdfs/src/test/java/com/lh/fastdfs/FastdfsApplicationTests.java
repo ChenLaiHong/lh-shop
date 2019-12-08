79b037a6e3429ea4d8d46f31f3adcf92ec2303b1
laiHom <1185630400@qq.com>
2019-12-08 22:21:00 +0800

    fastdfs

---
+++ lh-shop-temp/fastdfs/src/test/java/com/lh/fastdfs/FastdfsApplicationTests.java
@@ -0,0 +1,14 @@
+package com.lh.fastdfs;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastdfsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
